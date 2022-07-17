1513911ae8a8ca8ca2c1ef440b8e851ad63cdb60
Adarsha <vatraiadarsh@gmail.com>
2022-07-17 14:40:08 +1000

    added bundle configs and bootstrap 5

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Adarsh.EmployeeCRM.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/css/bootstrap")
+                .Include("~/Content/bootstrap.min.css"));
+
+            bundles.Add(new ScriptBundle("~/js/bootstrap")
+                .Include("~/Scripts/bootstrap.min.js"));
+        }
+    }
+}
--- Global.asax.cs
+++ Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Adarsh.EmployeeCRM.Web.App_Start;
+using System.Web.Optimization;
 
 namespace Adarsh.EmployeeCRM.Web
 {
@@ -13,6 +15,7 @@ namespace Adarsh.EmployeeCRM.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
